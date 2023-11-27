5d98144b711327af96acfae3e9d4bd81230838d9
aps8778 <121794926+aps8778@users.noreply.github.com>
2023-11-27 15:26:05 +0000

    datatypes

---
+++ Basics_01/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all code as newer version
+
+//alert(hello) // we are using node js, not browser.
+
+console.log(3 
+    + 3)// code readability should be high
+
+console.log("abhishek")  
+
+let name = "abhishek";
+let age = 4;
+let isLoggedIn = false;
+
+// number
+// bigInt
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object 
+
+console.log(typeof null);// object
+console.log(typeof undefined);//undefined 

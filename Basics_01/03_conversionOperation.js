74c30942fb8a4e365f37c2d1484d82a7caafb25d
aps8778 <121794926+aps8778@users.noreply.github.com>
2023-11-28 17:36:47 +0000

    convOp

---
+++ Basics_01/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = true;
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+//true = 1 , false =0
+
+let isLoggedIn = "" ;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(isLoggedIn);

1adb21b84d03b03efd21c0a0481f175cde39034e
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-01-07 05:50:17 +0000

    save

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score);
 // undefined => NaN
 //true = 1 , false =0
 
-let isLoggedIn = "" ;
+let isLoggedIn = "";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
@@ -21,7 +21,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
 //**********Operations*********
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%3);
+console.log(2**3);*/
+
+str1 = "Hello";
+str2 = " Abhishek";
+//console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);

ac43c854136c7ba489917e96f574c76b0f30dd8a
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-01-07 15:21:42 +0000

    Datatypes summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,28 @@
+// Primitive
+//7 types: String, Number, Boolean, null, undefined, Symbol, BigInt;
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const bigNumber = 2345775113456n
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+//Reference
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "Naagraj", "Comando Dhruv"];
+
+let myObj = 
+{
+    name:"abhishek",
+    age:22
+};
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof scoreValue);

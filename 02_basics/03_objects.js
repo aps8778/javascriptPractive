1906fdfd519a421fc1b029bb276be287efb3318f
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-01-13 18:43:13 +0000

    quick save

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+// singleton
+//object.create;
+
+
+//Object.literals
+const jsUser = {
+    name:"abhishek",
+    age:20,
+    location:"kripalpur",
+    email:"abhishek@goole.com",
+    isLoggedIn: false,
+    lastLoginDays:["Monday","Saturday"]
+}

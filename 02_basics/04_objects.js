c43cafea15c8938e988d9d41dd412068f49bd0ed
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-01-16 18:57:36 +0000

    save objects details

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,45 @@
+const tinderUser = new Object();
+
+tinderUser.id = "123abc";
+tinderUser.name = "shyam";
+tinderUser.isLoggedIn = false;
+
+//console.log(tinderUser);
+
+const regularUser = {
+  email: "abhishek@google.com",
+  fullName: {
+    userFullName: {
+      firstName: "abhishek",
+      lastName: "singh",
+    },
+  },
+};
+
+//console.log(regularUser.fullName.userFullName.lastName);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+
+//const obj3 = Object.assign({}, obj1, obj2);
+
+const obj3 = { ...obj1, ...obj2 };
+//console.log(obj3);
+
+const users = [
+    {
+
+    },{
+
+    },{
+
+    },{
+
+    }
+]
+
+//console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));

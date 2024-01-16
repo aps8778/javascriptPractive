acf913d9df0fb8f7474c7d7b89434d158ff0af03
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-01-16 16:38:32 +0000

    objects lesson

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,13 +1,37 @@
 // singleton
 //object.create;
 
-
 //Object.literals
+const mySym = Symbol("key1");
+
 const jsUser = {
-    name:"abhishek",
-    age:20,
-    location:"kripalpur",
-    email:"abhishek@goole.com",
-    isLoggedIn: false,
-    lastLoginDays:["Monday","Saturday"]
-}
+  name: "abhishek",
+  "full name": "Abhishek Pratap Singh",
+  [mySym]: "myKey2",
+  age: 20,
+  location: "kripalpur",
+  email: "abhishek@goole.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+//console.log(jsUser.email);
+//console.log(jsUser["email"]);
+//console.log(jsUser["full name"]);
+//console.log(jsUser."full name");
+//console.log(jsUser[mySym]);
+
+jsUser.email = "abhishek@chatgpt.com";
+
+//Object.freeze(jsUser);
+jsUser.email = "abhishek@microsoft.com";
+// console.log(jsUser);
+
+jsUser.greeting = function () {
+  console.log("Hello Js user");
+};
+jsUser.greetingTwo = function () {
+  console.log(`Hello Js user, ${this.name}`);
+};
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());

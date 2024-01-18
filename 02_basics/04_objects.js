ba113a158212e9450d25ede7d825c40b37a737f4
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-01-18 06:25:01 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,20 +26,22 @@ const obj2 = { 3: "a", 4: "b" };
 const obj3 = { ...obj1, ...obj2 };
 //console.log(obj3);
 
-const users = [
-    {
-
-    },{
-
-    },{
-
-    },{
-
-    }
-]
+const users = [{}, {}, {}, {}];
 
 //console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+const course = {
+  courseName: "js in hindi",
+  price: "999",
+  courseInstructor: "hitesh",
+};
+// course.courseInstructor
+
+const { courseInstructor: instructor } = course;
+
+//console.log(courseInstructor);
+console.log(instructor);

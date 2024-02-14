8f70aab99b9ccf2e4074297095da4f10842912db
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-02-14 11:53:17 +0000

    funtions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+function myName() {
+  console.log("A");
+  console.log("B");
+  console.log("H");
+  console.log("I");
+  console.log("S");
+  console.log("H");
+  console.log("E");
+  console.log("K");
+}
+// myName();
+
+// function addTwoNumbers(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// addTwoNumbers(6, 7);
+
+function addTwoNumbers(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+
+addTwoNumbers(6, 7);

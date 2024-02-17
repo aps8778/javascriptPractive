3c693621d20574668baf67f9c0dad254a4a13cff
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-02-17 10:38:08 +0000

    ok

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,21 +1,18 @@
 //var a = 30;
- //let b = 100;
+//let b = 100;
 // const c = 200;
 
-if (true){
-    var a = 300;
-    let b = 400;
-    const c = 200; 
-    //console.log(`INNER:`, b) 
+if (true) {
+  var a = 300;
+  let b = 400;
+  const c = 200;
+  //console.log(`INNER:`, b)
 }
 
 for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
+  const element = array[i];
 }
 
 //console.log(a);
 //console.log(b);
 //console.log(c);
-
-

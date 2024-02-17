344a9b5132e16c7f909d6fa24a7dd2ccde7bd72e
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-02-17 10:37:41 +0000

    scope lecture 1

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,21 @@
+//var a = 30;
+ //let b = 100;
+// const c = 200;
+
+if (true){
+    var a = 300;
+    let b = 400;
+    const c = 200; 
+    //console.log(`INNER:`, b) 
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+

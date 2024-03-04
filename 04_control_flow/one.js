9207d444a9ac96fb168dcf431ac86067adfaaf92
aps8778 <121794926+aps8778@users.noreply.github.com>
2024-03-04 09:24:34 +0000

    ok

---
+++ 04_control_flow/one.js
@@ -0,0 +1,11 @@
+// if
+
+// < ,> ,= ,<= ,>= ,== ,!= ,=== ,!==
+const userIsLoggedIn = true;
+const temperature = 40;
+
+if (temperature === 40) {
+  console.log("less than 50");
+} else {
+  console.log("temperature is greater than 50");
+}
